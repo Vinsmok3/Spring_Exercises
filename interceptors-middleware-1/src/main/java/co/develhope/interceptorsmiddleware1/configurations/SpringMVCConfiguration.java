09531169a161d367ebc7b2d32c650b662067bb52
Greed <cristiancoltea97@gmail.com>
2023-02-28 11:06:26 +0100

    Middleware_01

--- interceptors-middleware-1/src/main/java/co/develhope/interceptorsmiddleware1/configurations/SpringMVCConfiguration.java
+++ interceptors-middleware-1/src/main/java/co/develhope/interceptorsmiddleware1/configurations/SpringMVCConfiguration.java
@@ -3,6 +3,7 @@ package co.develhope.interceptorsmiddleware1.configurations;
 import co.develhope.interceptorsmiddleware1.interceptors.APILoggingInterceptor;
 import co.develhope.interceptorsmiddleware1.interceptors.LegacyInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
