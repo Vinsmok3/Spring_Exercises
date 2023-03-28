799f0fe4ee5912dec4f60a41e8b1970080efb10c
Greed <cristiancoltea97@gmail.com>
2023-03-28 13:06:38 +0200

    EnvironmentVariables02-DONE

---
+++ environment-variables-2/src/main/java/co/develhope/environmentvariables2/controllers/MainController.java
@@ -0,0 +1,17 @@
+package co.develhope.environmentvariables2.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @Value("${custom.welcomeMsg}")
+    private String welcomeMsg;
+
+    @GetMapping("/")
+    public String greeting() {
+        return welcomeMsg;
+    }
+}
